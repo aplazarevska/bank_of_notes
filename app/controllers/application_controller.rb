628dc7da8795bd168cd35abfa7e2cc4e7cc14d7d
Ana Lazarevska <aplazarevska@gmail.com>
2021-08-09 12:13:30 +1000

    improved few route issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,17 +5,5 @@ class ApplicationController < ActionController::Base
     @user_type = params[:user_type]
   end
 
-  def after_sign_in_path_for(profile)
-    if(current_user.profile)
-      if(params[:user][:user_type] == "buyer")
-        root_path
-      else
-        listings_path
-      end
-    elsif(params[:user][:user_type] == "returning_user")
-        root_path
-    else
-      new_profile_path(user_type: params[:user][:user_type]) || root_path
-    end
-  end
+ 
 end
--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -27,7 +27,7 @@ class PaymentController < ApplicationController
         }],
         mode: 'payment',
       success_url: "#{root_path}/listings/#{@listing.id}?checkout=success",
-      cancel_url: "#{root_path}",
+      cancel_url: "#{root_path}/listings/#{@listing.id}",
     })
 
     redirect_to session.url
