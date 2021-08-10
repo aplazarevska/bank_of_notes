930afc1e62e286f374f0c13547e3e8191904f1d8
Ana Lazarevska <aplazarevska@gmail.com>
2021-08-11 09:15:32 +1000

    added api keys for stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,1 +1,9 @@
-Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
+
+# Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
+
+Rails.configuration.stripe = {
+:stripe_public_key => ENV['STRIPE_PUBLIC_KEY'],
+:stripe_api_key => ENV['STRIPE_API_KEY']
+}
+
+Stripe.api_key = Rails.configuration.stripe[:stripe_api_key]
