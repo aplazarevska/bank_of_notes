6ca3030141eeba7c89ae3876609a60d21e33ed47
Ana Lazarevska <aplazarevska@gmail.com>
2021-08-10 16:08:02 +1000

    added custom validation method

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -9,4 +9,14 @@ class Listing < ApplicationRecord
   # each listing has to have price and images
   validates :price, presence: true
   validates :images, presence: true
+  
+  # validation custom method
+  before_validation :number_of_images
+
+  # custom validation to limit number of images that are uploaded to 2 and throws an error if otherwise
+  def number_of_images
+    if self.images.length != 2
+      errors.add :base, :invalid, message: "Please upload two images, obverse and reverse!"
+    end
+  end
 end
